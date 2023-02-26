c23b88fec3cdd8705c0adf8ee067e3103d091b1b
cching01099 <cching01099@gmail.com>
2023-02-26 18:16:36 +0800

    state of currentPhase setting in parent component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,9 +8,9 @@ import Carts from "./Carts/Cart";
 import { useState } from "react";
 
 function Main() {
-  //會變動的是每一step的狀態，所以先設定currentStep作為狀態變化的值，以及設定初始值為1
-  //傳遞值：data-phase的值，現在是到了哪一階段(currentStep)＝切換步驟事件
-  const [currentStep, setCurrentStep] = useState(1);
+  //會變動的是每一step的狀態，所以先設定currentPhase作為狀態變化的值，以及設定初始值為1
+  //傳遞值：data-phase的值，現在是到了哪一階段(currentPhase)＝切換步驟事件
+  const [currentPhase, setCurrentPhase] = useState(1);
 
   return (
     <main className="site-main">
@@ -18,11 +18,11 @@ function Main() {
         {/* data-phase值的更動(現在到了哪一階段），以去連動StepProgress與表單顯示*/}
         <section
           className="register-container col col-lg-6 col-sm-12"
-          data-phase={currentStep}
+          data-phase={currentPhase}
           data-total-price="0"
         >
           <h2 className="register-title col col-12">結帳</h2>
-          <StepProgress currentStep={currentStep} />
+          <StepProgress currentPhase={currentPhase} />
           <section className="form-container col col-12">
             <Step1 />
             <Step2 />
@@ -32,8 +32,8 @@ function Main() {
         <Carts />
         {/* data-phase值的更動(現在到了哪一階段），會去連動切換鈕呈現、切換步驟事件 */}
         <ProgressControl
-          currentStep={currentStep}
-          setCurrentStep={setCurrentStep}
+          currentPhase={currentPhase}
+          setCurrentPhase={setCurrentPhase}
         />
       </div>
     </main>
