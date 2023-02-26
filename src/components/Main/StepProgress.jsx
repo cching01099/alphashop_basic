0598942fc480f4843ea5cd8ed5172318e66a8684
cching01099 <cching01099@gmail.com>
2023-02-26 18:26:02 +0800

    stepProgress元件化

--- src/components/Main/StepProgress.jsx
+++ src/components/Main/StepProgress.jsx
@@ -1,40 +1,33 @@
 import { ReactComponent as PgCompleteIcon } from "../../icons/pg-complete.svg";
 
-export default function StepProgress({ currentStep }) {
+//三個重複類似的架構拉出來：content、bar條
+//progress裡面的Content
+function ProgressContent({ phase, stepNum, label }) {
   return (
-    <>
-      {/* <!-- register-progress --> */}
-      <section className="progress-container col col-12">
-        <span className="progress-group" data-phase="address">
-          <span className="progress-icon">
-            <span className="text">1</span>
-            <svg className="icon cursor-point">
-              <PgCompleteIcon />
-            </svg>
-          </span>
-          <span className="progress-label">寄送地址</span>
-        </span>
-        <span className="progress-bar" data-order="1"></span>
-        <span className="progress-group" data-phase="shipping">
-          <span className="progress-icon">
-            <span className="text">2</span>
-            <svg className="icon cursor-point">
-              <PgCompleteIcon />
-            </svg>
-          </span>
-          <span className="progress-label">運送方式</span>
-        </span>
-        <span className="progress-bar" data-order="2"></span>
-        <span className="progress-group" data-phase="credit-card">
-          <span className="progress-icon">
-            <span className="text">3</span>
-            <svg className="icon cursor-point">
-              <PgCompleteIcon />
-            </svg>
-          </span>
-          <span className="progress-label">付款資訊</span>
-        </span>
-      </section>
-    </>
+    <span className="progress-group" data-phase={phase}>
+      <span className="progress-icon">
+        <span className="text">{stepNum}</span>
+        <svg className="icon cursor-point">
+          <PgCompleteIcon />
+        </svg>
+      </span>
+      <span className="progress-label">{label}</span>
+    </span>
+  );
+}
+// progress的bar條
+function ProgressBar({ stepNum }) {
+  return <span className="progress-bar" data-order={stepNum}></span>;
+}
+
+export default function StepProgress() {
+  return (
+    <section className="progress-container col col-12">
+      <ProgressContent phase="address" stepNum={1} label="寄送地址" />
+      <ProgressBar stepNum={1} />
+      <ProgressContent phase="shipping" stepNum={2} label="運送方式" />
+      <ProgressBar stepNum={2} />
+      <ProgressContent phase="credit-card" stepNum={3} label="付款資訊" />
+    </section>
   );
 }
